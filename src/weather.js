318e7fd37f2b07db6e5937642f51e241d0784c21
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-26 22:23:10 +0600

    Refactor processing of weather data

--- src/weather.js
+++ src/weather.js
@@ -37,43 +37,17 @@ export default async function processData() {
   }
 
   const processedData = {
-    current: {},
-    hourly: [],
-    daily: [],
+    current: pick(data.currentConditions, selectedCurrentData),
+    hourly: data.days[0].hours.map((hour) => pick(hour, selectedHourlyData)),
+    daily: data.days.map((day) => pick(day, selectedDailyData)),
   };
 
-  const current = processedData['current'];
-  const daily = processedData['daily'];
-  const hourly = processedData['hourly'];
-
-  const unprocessedCurrentData = data['currentConditions'];
-  const unprocessedDays = data['days'];
-  const unprocessedHours = unprocessedDays[0]['hours'];
-
-  Object.entries(unprocessedCurrentData).forEach(([key, value]) => {
-    if (selectedCurrentData.includes(key)) {
-      current[key] = value;
-    }
-  });
-  console.log(current);
-
-  unprocessedDays.forEach((day) => {
-    daily.push({});
-    Object.entries(day).forEach(([key, value]) => {
-      if (selectedDailyData.includes(key)) {
-        daily[daily.length - 1][key] = value;
-      }
-    });
-  });
-
-  unprocessedHours.forEach((hour) => {
-    hourly.push({});
-    Object.entries(hour).forEach(([key, value]) => {
-      if (selectedHourlyData.includes(key)) {
-        hourly[hourly.length - 1][key] = value;
-      }
-    });
-  });
-
   console.log(processedData);
+}
+
+function pick(obj, keys) {
+  return keys.reduce((acc, key) => {
+    acc[key] = obj[key];
+    return acc;
+  }, {});
 }

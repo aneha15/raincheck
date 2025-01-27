848c2347a1a55111103ea6f397240e22a4c6ebc3
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-27 14:44:35 +0600

    Add search bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './style.css';
+import processData from './weather';
+
+function handleSearch(location) {
+  console.log(location);
+  processData(location);
+}
+
+function searchLocation() {
+  // const form = document.getElementById('search-location');
+  const locationInput = document.getElementById('location');
+  const searchIcon = document.getElementById('search-icon');
+
+  // console.log(locationInput.value);
+  searchIcon.addEventListener('click', () => handleSearch(locationInput.value));
+  locationInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      searchIcon.click();
+    }
+  });
+}
+
+searchLocation();
+
+// getData('switzerland');
+// processData();
